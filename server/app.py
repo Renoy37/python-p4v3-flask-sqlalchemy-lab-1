cd4a674bf3fe27a04762c04c4bad5bb47364fa97
Joy Kithinji Gatwiri <jgatwiri@jacarandahealth.org>
2024-04-05 21:17:59 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -21,6 +21,47 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+
+    if earthquake:
+        earthquake_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        return make_response(earthquake_data, 200)
+    else:
+        return make_response({'message': f'Earthquake {id} not found.'}, 404)
+    
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    count = len(earthquakes)
+    earthquakes_data = []
+
+    for earthquake in earthquakes:
+        earthquake_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        earthquakes_data.append(earthquake_data)
+
+    response_data = {
+        'count': count,
+        'quakes': earthquakes_data 
+    }
+
+    return make_response(response_data, 200)
+
+
 
 
 if __name__ == '__main__':
